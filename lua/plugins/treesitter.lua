04fd5dacb4d11b4569b4d2e75c22813090002259
Andrea Bigagli <abigagli@gmail.com>
2026-05-06 11:16:20 +0200

    Disable frozen nvim-treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,9 +2,17 @@
 
 -- Customize Treesitter
 
+-- NOTE: I TRIED TO FOLLOW https://www.reddit.com/r/neovim/comments/1san03g/treesitter_migration_guide_for_nvim_012_to/
+-- and https://www.qu8n.com/posts/treesitter-migration-guide-for-nvim-0-12
+-- but gave up and just disabled this plugin and started using treesitter-manager
+
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "main",
+  commit = "HEAD",
+  lazy = false,
+  enabled = false,
   opts = {
     ensure_installed = {
       "lua",
